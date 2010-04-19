85c946807a16636a56c4749e087462bad2d738a0
Maerlyn <maerlyng+github@gmail.com>
2010-04-19 21:58:07 +0200

    improving news pagination

--- apps/frontend/modules/news/templates/indexSuccess.php
+++ apps/frontend/modules/news/templates/indexSuccess.php
@@ -2,19 +2,29 @@
 <?php use_helper("Date") ?>
 <?php include_partial("list", array("news" => $pager->getResults())) ?>
 
-<?php if ($pager->haveToPaginate()): ?>
+<?php if ($pager->haveToPaginate()): /* @var $pager sfPropelPager */?>
   <div class="pagination">
-    <a href="<?php echo url_for("@news_index?page=1") ?>">&laquo;</a> 
+    <?php if (!$pager->isFirstPage()): ?>
+    <a href="<?php echo url_for("@news_index?page=1") ?>">&laquo;</a>
     <a href="<?php echo url_for("@news_index?page=" . $pager->getPreviousPage()) ?>">&lsaquo;</a>
-    <?php foreach ($pager->getLinks() as $page): ?>
+    <?php endif ?>
+
+    <?php $is_first = true; foreach ($pager->getLinks() as $page): ?>
+      <?php if ($is_first && $page != 1) { echo "…"; } $is_first = false; ?>
+
       <?php if ($page == $pager->getPage()): ?>
         <b><?php echo $page ?></b>
       <?php else: ?>
-        <a href="<?php echo url_for("@news_index?page=" . $page) ?>"><?php echo $page ?></a> 
+        <a href="<?php echo url_for("@news_index?page=" . $page) ?>"><?php echo $page ?></a>
       <?php endif ?>
-    <?php endforeach ?>
-    
-    <a href="<?php echo url_for("@news_index?page=" . $pager->getNextPage()) ?>">&rsaquo;</a> 
+
+    <?php $last = $page; endforeach; ?>
+
+    <?php if ($last != $pager->getLastPage()) { echo "…"; } ?>
+
+    <?php if (!$pager->isLastPage()): ?>
+    <a href="<?php echo url_for("@news_index?page=" . $pager->getNextPage()) ?>">&rsaquo;</a>
     <a href="<?php echo url_for("@news_index?page=" . $pager->getLastPage()) ?>">&raquo;</a>
+    <?php endif ?>
   </div>
 <?php endif ?>
