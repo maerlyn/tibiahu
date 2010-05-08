6d5feb583058a8603baeaeafa11232151b911f93
Maerlyn <maerlyng+github@gmail.com>
2010-05-08 16:34:16 +0200

    removing info leak in thu_menu() helper

--- lib/helper/TibiahuHelper.php
+++ lib/helper/TibiahuHelper.php
@@ -2,6 +2,6 @@
 
 function thu_menu($name, $uri, $options = array())
 {
-  echo "<!-- " . sfContext::getInstance()->getRouting()->getCurrentInternalUri(true) . " " . $uri . "--> ";
+  //echo "<!-- " . sfContext::getInstance()->getRouting()->getCurrentInternalUri(true) . " " . $uri . "--> ";
   return link_to_unless(strpos(sfContext::getInstance()->getRouting()->getCurrentInternalUri(true), $uri) !== false, $name, $uri, $options);
 }
